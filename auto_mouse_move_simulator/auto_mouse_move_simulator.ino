e20c7e943c68f5c9caec38c83f9cb72e3429b17b
Renie Ak <code@renieak.com>
2021-11-08 01:50:26 +0530

    Helpful coments added

--- auto_mouse_move_simulator/auto_mouse_move_simulator.ino
+++ auto_mouse_move_simulator/auto_mouse_move_simulator.ino
@@ -10,23 +10,24 @@
 const int buttonPin = 12;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(buttonPin, INPUT_PULLUP);
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP); // PIN to enable/disable the auto mouse movement
+  pinMode(LED_BUILTIN, OUTPUT);     // used to indicate the program activity
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(LED_BUILTIN, LOW); // No LED light when no mouse movement
   
   int time_count, time_interval;
   time_count = time_interval = 50; // time_interval is in seconds
   
   while(digitalRead(12) == HIGH){
-    if(time_count > time_interval){
-      time_count = 0;
-      digitalWrite(LED_BUILTIN, HIGH);
+    if(time_count > time_interval){ // Mouse moves after time_interval
+      time_count = 0;   //resets time_interval count
+      
+      digitalWrite(LED_BUILTIN, HIGH);  // LED light ON when mouse moves
+      
       Mouse.move(0,100);  //down
       delay(1000);
       Mouse.move(0,-75); //up
@@ -38,10 +39,11 @@ void loop() {
       Mouse.move(-50,0);  //left
       delay(1000);
       Mouse.move(0,-25); //up
-      digitalWrite(LED_BUILTIN, LOW);
+      
+      digitalWrite(LED_BUILTIN, LOW); // No LED light when no mouse movement
     }
-    time_count++;
-    delay(1000);
+    time_count++; // incrementing time_count
+    delay(1000);  // time_interval second for every count
   }
 
 }
